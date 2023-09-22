51f4f0778dc19f9907a77c8870d453dc032ed73e
Salim B <git@salim.space>
2023-09-22 02:02:20 +0200

    multiple changes:
    - add `netlify_dns_records_set()`
    - tweak `paths_to_keep` (for deploy fns)
    - switch to  `pal::cli_progress_step_quick()`
    - update lintr config
    -

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,6 +1,7 @@
 list(
   rd_family_title = list(gh      = "Other GitHub functions:",
                          gitrepo = "Other Git repository functions:",
+                         netlify = "Other Netlify functions:",
                          regex   = "Other regular expression rules:",
                          string  = "Other string functions:",
                          tibble  = "Other data frame / tibble functions:")
