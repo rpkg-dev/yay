595d3a7803d7919771ad611eab96398a13218f8a
Salim B <git@salim.space>
2023-10-15 23:15:11 +0200

    add `zotero_write_bib()`

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -4,5 +4,6 @@ list(
                          netlify = "Other Netlify functions:",
                          regex   = "Other regular expression rules:",
                          string  = "Other string functions:",
-                         tibble  = "Other data frame / tibble functions:")
+                         tibble  = "Other data frame / tibble functions:",
+                         zotero  = "Other Zotero functions:")
 )
