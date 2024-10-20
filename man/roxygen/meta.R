803514163ece4cd2e98d4b19f313664a34d92eea
Salim B <git@salim.space>
2024-10-20 11:40:35 +0200

    fix: add missing roxygen family tag def

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -2,6 +2,7 @@ list(
   rd_family_title = list(gh      = "Other GitHub functions:",
                          gitrepo = "Other Git repository functions:",
                          netlify = "Other Netlify functions:",
+                         porkbun = "Other Porkbun functions:",
                          regex   = "Other regular expression rules:",
                          string  = "Other string functions:",
                          tibble  = "Other data frame / tibble functions:",
