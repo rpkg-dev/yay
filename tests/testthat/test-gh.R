2a25f07ed55915b6be6ab000db3a288f5093cd00
Salim B <git@salim.space>
2025-03-02 18:48:44 +0100

    test: update repo URLs

--- tests/testthat/test-gh.R
+++ tests/testthat/test-gh.R
@@ -1,19 +1,19 @@
 # gh_dir_ls ----
 test_that("`gh_dir_ls()` basically works", {
   expect_identical(yay::gh_dir_ls(path = "README.md",
-                                  owner = "salim-b",
+                                  owner = "rpkg-dev",
                                   name = "pal"),
                    character())
 
   expect_identical(yay::gh_dir_ls(path = "tests",
-                                  owner = "salim-b",
+                                  owner = "rpkg-dev",
                                   name = "pal",
                                   recurse = FALSE,
                                   incl_dirs = FALSE),
                    "tests/testthat.R")
 
   expect_identical(yay::gh_dir_ls(path = "tests",
-                                  owner = "salim-b",
+                                  owner = "rpkg-dev",
                                   name = "pal",
                                   recurse = FALSE,
                                   incl_files = FALSE),
